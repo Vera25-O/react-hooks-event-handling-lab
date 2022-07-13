a3695057bfe68550c60439f87dadfc437b3dfaab
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-07-13 17:00:27 +0300

    updated EyesOnMe.js

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+  function myEvent(event){
+
+   event.preventDefault();
+  
+console.log("Good!")
+  }
+  return (
+  <button onClick={myEvent}>Eyes on me</button>
+  )
+}
+
+export default EyesOnMe;
