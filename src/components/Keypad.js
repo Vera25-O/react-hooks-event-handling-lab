ce5f138f9dc433e2e62fbacf72a1264243d31d59
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-07-13 16:59:48 +0300

    commit tree 6805519186c18e2a2810eded350ee7edc0b13106

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
 // Code Keypad Component Here
+import React from 'react';
+
+
+function Keypad(){
+function eventHandler(event){
+  
+  event.preventDefault();
+  console.log('Entering password...')
+}
+return<form onChange={eventHandler}>
+<input type="password" name="password" placeholder="Enter password..." />
+</form>
+}
+    
+
+
+export default Keypad;
